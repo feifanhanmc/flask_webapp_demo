519d9c2ab793174eeb076dc2935ef43c69da3cb6
feifanhanmc <feifanhanmc@163.com>
2018-11-04 19:10:55 +0800

    test danmu

--- web/webapp/index/views.py
+++ web/webapp/index/views.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, url_for, render_template, request,\
                   abort, flash, session, redirect
 import json
-
+import requests
 
 mod = Blueprint('index', __name__, url_prefix='/')
 
@@ -15,3 +15,8 @@ def index():
 def huaji():
     return render_template('huaji.html')
 
+@mod.route('/decode_danmu')
+def decode_danmu():
+    danmu_url = 'http://cmts.iqiyi.com/bullet/77/00/874217700_300_3.z?business=danmu&is_iqiyi=true&is_video_page=true&tvid=874217700&albumid=205025001&categoryid=2'
+    r = requests.get(danmu_url)
+    return r.text
